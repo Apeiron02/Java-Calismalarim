0d1a90929d82a546da878f928b23864f30176f97
Muhammed Yusuf Karaman <116491576+Apeiron02@users.noreply.github.com>
2023-05-05 16:35:23 +0300

    Delete src/main/java directory

--- src/main/java/Yildizpramit.java
+++
@@ -1,9 +0,0 @@
-public class Yildizpramit {
-    public static void main(String[] args) {
-        int yukseklik = 10;
-        int satir = 10;
-        for (int i =1 ;i<=yukseklik;i++){
-
-        }
-    }
-}
